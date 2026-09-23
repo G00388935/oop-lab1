635fba4d633875758aa1466739953ebbb5f8d6b6
KevinRooney-STUDENT <g00388935@atu.ie>
2026-09-23 17:34:05 +0100

    Added borrowBook method to book

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 Book Tracker application
+
+Lab 1 
+lists book title, author, page count and availability
+from listed books stored
+
+JDK version 24 run in intellij in main.java
+
+object class is called "book"
+
+Book Title: Dune
+Book Author: Frank Herbert
+Book Page count: 412
+Book Available: true
+Dune has been borrowed
+Book Title: Dune
+Book Author: Frank Herbert
+Book Page count: 412
+Book Available: false
--- src/main/java/ie/atu/oop/week1/Main.java
+++ src/main/java/ie/atu/oop/week1/Main.java
@@ -12,5 +12,25 @@ package ie.atu.oop.week1;
         firstbook.displayDetails();//before
         firstbook.borrowBook();
         firstbook.displayDetails();//after
+
+        book secondbook = createBook("Clean Code","Dan Williams",223);
+        book thirdbook = createBook("Another Book Code","John Murphy",333);
+        book fourthbook = createBook("Student Life","Mary Shelly",443);
+
+        System.out.println("\n");
+        secondbook.displayDetails();
+        System.out.println("\n");
+        thirdbook.displayDetails();
+        System.out.println("\n");
+        fourthbook.displayDetails();
+
+    }
+    private static book createBook(String title, String author, int pageCount)
+    {
+        book book = new book();
+        book.title = title;
+        book.author = author;
+        book.pageCount = pageCount;
+        return book;
     }
 }
